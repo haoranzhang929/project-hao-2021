2b3c322334844c964fca4e203d90cbf482496fe1
Haoran Zhang <haoranzhang@icloud.com>
2021-03-27 22:11:10 +0000

    update test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders test text", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText("Init");
-  expect(linkElement).toBeInTheDocument();
+  const testElement = getByText("Init");
+  expect(testElement).toBeInTheDocument();
 });
